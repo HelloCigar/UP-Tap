4c7149b4e0cb81eb5d95cd97b4dd1baf29c2d95e
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2024-11-21 11:31:42 +0800

    postgre update

--- UP_Tap/settings.py
+++ UP_Tap/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#4-r355t&9w0@1c6=m1lq*)xan31b#!h6dgz3v*_@2f*c0qp2'
+# SECURITY WARNING: keep the secret key used in production secret!, see https://djecrety.ir/
+SECRET_KEY = os.getenv('DJANGO_SECRETKEY', 'django-insecure-)#4-r355t&9w0@1c6=m1lq*)xan31b#!h6dgz3v*_@2f*c0qp2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
